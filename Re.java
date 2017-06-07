41e80f186dee888e9365c5061d466da11db93986
logumuruganThiyagarajan <logucs20@gmail.com>
2017-06-07 18:37:03 +0530

    Create Re.java

---
+++ Re.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Re
+{
+    public static void main(String muruga[])
+    
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        StringBuffer buffer = new StringBuffer(a);
+        buffer.reverse();
+        System.out.println(buffer);
+        
+        
+        
+    }
+       
+    
+    
+    
+}
