540d75933cb50488113723c6df496050143f00c8
logumuruganThiyagarajan <logucs20@gmail.com>
2017-06-13 11:58:26 +0530

    Create oddev.java

---
+++ oddev.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class oddev
+{
+    public static void main(String muru[])
+{
+    Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+    if(a%2==0)
+    {
+        System.out.println("Given no is Even");
+    }
+    else
+    {
+        System.out.println("Given no is Odd");
+    }
+}    
+}
