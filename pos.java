00a98717416fe3a0c47c9c7ce9827aecf68aa36a
logumuruganThiyagarajan <logucs20@gmail.com>
2017-06-13 11:50:19 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class pos
+{
+    public static void main(String muru[])
+{
+    Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+    if(a>0)
+    {
+        System.out.println("Given no is Positive");
+    }
+    else
+    {
+        System.out.println("Given no is Negative");
+    }
+}    
+}
