94cdc2a5b43cc001154c7d6949e7adafcb8464c2
logumuruganThiyagarajan <logucs20@gmail.com>
2017-06-13 12:03:44 +0530

    Create helo.java

---
+++ helo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class helo
+{
+    public static void main(String muru[])
+{
+    Scanner sc=new Scanner(System.in);
+      String a=sc.next();
+      for(int i=0;i<5;i++)
+      {
+          System.out.println(a);
+      }
+    
+}    
+}
