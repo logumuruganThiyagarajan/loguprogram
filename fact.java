5fbf3c7102b721364d60c8bd7dbab0516f5826af
logumuruganThiyagarajan <logucs20@gmail.com>
2017-06-13 12:24:19 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class fact
+{
+    public static void main(String muru[])
+{
+    Scanner sc=new Scanner(System.in);
+    int f=1;
+      int a=sc.nextInt();
+      
+      for(int i=a;i>0;i--)
+      {
+          f=f*i;
+      }
+      System.out.println(f);
+      
+     
+     
+}    
+}
