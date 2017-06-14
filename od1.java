85f0cfb4789bf51fb36a6c49028fb16d09e63da8
logumuruganThiyagarajan <logucs20@gmail.com>
2017-06-14 09:59:17 +0530

    Create od1.java

---
+++ od1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class od1
+{
+    public static void main(String muruga[])
+    
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a%2==0)
+        {
+            System.out.println("No is Even");
+        }
+        else
+        {
+            System.out.println("No is Odd");
+        }
+         }
+       }
+
