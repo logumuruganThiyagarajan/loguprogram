b636a2e892a6bd93f9359d8680f625b2720af805
logumuruganThiyagarajan <logucs20@gmail.com>
2017-06-14 17:56:42 +0530

    Create le.java

---
+++ le.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class le
+{
+    public static void main(String[] args) 
+    {
+        int n,summ=0;
+        Scanner s = new Scanner(System.in);
+       String s1=s.next();
+     int len=s1.length();
+       
+        
+        System.out.println(len);
+    }
+}
