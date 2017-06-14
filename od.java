5a13d22bbfdb503a46b9374996c3b9912a4fbb76
logumuruganThiyagarajan <logucs20@gmail.com>
2017-06-14 09:55:36 +0530

    Create od.java

---
+++ od.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class od
+{
+    public static void main(String muruga[])
+    
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>0)
+        {
+            System.out.println("No is Positive");
+        }
+        else
+        {
+            System.out.println("No is Negative");
+        }
+         }
+       }
+
