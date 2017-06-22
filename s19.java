650d37bc247418b2ef77aeeb553111b1bde164ba
logumuruganThiyagarajan <logucs20@gmail.com>
2017-06-22 14:13:46 +0530

    Create s19.java

---
+++ s19.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class s19
+{
+    public static void main(String muru[])
+    {
+         Scanner sc=new Scanner(System.in);
+        int a,b,p=1,i;
+        a=sc.nextInt();
+        for(i=1;i<=a;i++)
+        {
+            p=p*i;
+        }
+        System.out.println("Factorial is="+p);
+    
+    }
+    
+}
+
+
